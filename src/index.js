ed6cf673224e5c073f397aa8bb27b347527d648c
nkhiljee <51273072+nkhiljee@users.noreply.github.com>
2019-08-07 16:46:52 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
-// function displayImg() {
-
-// } 
-
 fetch(imgUrl)
     .then(res => res.json())
     .then(data => {
@@ -16,7 +12,7 @@ fetch(imgUrl)
             img.src = element
             div.append(img)
         });
-    })
+})
 
 fetch(breedUrl)
     .then(res => res.json())
@@ -27,11 +23,10 @@ fetch(breedUrl)
         option.addEventListener("change", function(event){
         const startsWithN = breed.filter((breed) => breed.startsWith(event.target.value))
         console.log(startsWithN)
-        // console.log(breed)
+
         const ul = document.querySelector("#dog-breeds")
         ul.innerHTML = ""
         startsWithN.forEach(element =>{
-            // 
 
             const li = document.createElement("li")
             li.innerText = element
